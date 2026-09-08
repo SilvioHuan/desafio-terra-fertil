b0f41610db481dff73bf3a63916cd2e33926525b
Silvio Huan Matheus Grein Cristo Ribeiro <silviohuan@gmail.com>
2026-09-08 12:36:04 -0300

    Primeiro commit

---
+++ rateio.ts
@@ -0,0 +1,211 @@
+
+export const MUDAS_POR_BANDEJA = 50;
+
+export type SituacaoCadastral = "regular" | "suspensa" | "irregular";
+
+export interface Associacao {
+    cnpj: string;
+    nome: string;
+    municipio: string;
+    familias: number;
+    cotaMaxima: number;
+    situacao: SituacaoCadastral;
+}
+
+export interface Distribuicao {
+    cnpj: string;
+    nome: string;
+    bandejas: number;
+    mudas: number;
+    motivoExclusao?: string;
+}
+
+export interface ResultadoRateio {
+    distribuicao: Distribuicao[];
+    totalDistribuido: number;
+    sobraNaoDistribuida: number;
+}
+
+interface DistribuicaoComPontuacao extends Distribuicao {
+    pontuaco: number;
+    familias: number;
+    bandejaLimite: number;
+    situacao:SituacaoCadastral
+}
+
+export class RateioError extends Error {
+
+}
+
+export function ratearMudas(totalMudas: number, associados: Associacao[]): ResultadoRateio {
+
+    //const associadosAprovados = associados.filter(associado => associado.situacao === "regular" && associado.familias > 0);
+
+    let loteDisponivel = Math.floor(totalMudas / MUDAS_POR_BANDEJA);
+
+    const sobraNaoDistribuida = (totalMudas - (loteDisponivel * MUDAS_POR_BANDEJA));
+
+    const somaDasFamilias = associados.reduce((familiasSomada, familiasAtuais) => familiasSomada + familiasAtuais.familias, 0)
+
+    const distribuicaoAssociados: DistribuicaoComPontuacao[] = associados.map(associacao => {
+        
+        if(associacao.situacao !== "regular") {
+            
+            const distribuicao:DistribuicaoComPontuacao = {
+                nome:associacao.nome,
+                cnpj:associacao.cnpj,
+                bandejas:0,
+                mudas:0,
+                pontuaco:0,
+                familias:associacao.familias,
+                bandejaLimite:0,
+                situacao:associacao.situacao,
+                motivoExclusao: associacao.situacao
+            }            
+            return distribuicao
+        }
+
+        const cotaIdeal = Math.floor(totalMudas / MUDAS_POR_BANDEJA) * associacao.familias / somaDasFamilias
+
+        const bandejaLimite = cotaIdeal > 0 ? Math.floor(associacao.cotaMaxima / MUDAS_POR_BANDEJA): cotaIdeal
+
+        const bandejaEncontrada = Math.floor(cotaIdeal) > bandejaLimite ? bandejaLimite : Math.floor(cotaIdeal)
+
+        const bandejaDisponivel = loteDisponivel > bandejaEncontrada ? bandejaEncontrada : loteDisponivel;
+
+
+        loteDisponivel = loteDisponivel > bandejaEncontrada ? loteDisponivel - bandejaEncontrada : 0
+
+        console.log(bandejaLimite)
+
+        const distribuicao: DistribuicaoComPontuacao = {
+            cnpj: associacao.cnpj,
+            nome: associacao.nome,
+            bandejas: bandejaDisponivel,
+            mudas: bandejaDisponivel * MUDAS_POR_BANDEJA,
+            pontuaco: cotaIdeal,
+            familias: associacao.familias,
+            bandejaLimite: bandejaLimite,
+            situacao:"regular"
+        }
+        return distribuicao
+    });
+
+
+    if (loteDisponivel > 0 && distribuicaoAssociados.length > 0) {
+
+        const associadosSemBandeja: DistribuicaoComPontuacao[] = distribuicaoAssociados.filter(associacao => associacao.bandejas < associacao.bandejaLimite && associacao.situacao === "regular")
+
+
+        if (associadosSemBandeja.length > 0) {
+
+            for (let i: number = 0; i < associadosSemBandeja.length; i++) {
+
+                let trocou = false;
+
+                for (let j: number = 0; j < associadosSemBandeja.length - 1; j++) {
+
+                    const atual = associadosSemBandeja[j];
+
+                    const proximo = associadosSemBandeja[j + 1];
+
+                    if (proximo.pontuaco < atual.pontuaco) {
+                        trocou = true
+                        const temporario = associadosSemBandeja[j];
+                        associadosSemBandeja[j] = associadosSemBandeja[j + 1];
+                        associadosSemBandeja[j + 1] = temporario;
+
+                        continue
+                    }
+
+                    if (proximo.pontuaco == atual.pontuaco) {
+
+                        if (proximo.familias < atual.familias) {
+                            trocou = true
+                
+                            const temporario = associadosSemBandeja[j];
+                            associadosSemBandeja[j] = associadosSemBandeja[j + 1];
+                            associadosSemBandeja[j + 1] = temporario;
+                            continue
+                        }
+
+                        const nomeOrdenacao = proximo.nome.localeCompare(atual.nome, "pt-BR", { sensitivity: "base" })
+
+                        if (nomeOrdenacao < 0) {
+                            trocou = true
+                            
+                            const temporario = associadosSemBandeja[j];
+                            associadosSemBandeja[j] = associadosSemBandeja[j + 1];
+                            associadosSemBandeja[j + 1] = temporario;
+                            continue
+                        }
+
+                        const cnpjOrdenacao = proximo.cnpj.localeCompare(atual.cnpj, "pt-BR", { sensitivity: "base" })
+
+                        if (cnpjOrdenacao < 0) {
+                            trocou = true
+                           
+                            const temporario = associadosSemBandeja[j];
+                            associadosSemBandeja[j] = associadosSemBandeja[j + 1];
+                            associadosSemBandeja[j + 1] = temporario;
+                            continue
+                        }
+                    }
+
+                }
+
+                if (!trocou) {
+                    break
+                }
+            }
+
+            let index = 0;
+
+            let bandejaAdicionada = false
+            while (loteDisponivel > 0) {
+
+                if(associadosSemBandeja.length == index) {
+                    if(!bandejaAdicionada) {
+                        break
+                    }
+                    index = 0
+                    bandejaAdicionada = false
+                }
+
+                if (associadosSemBandeja[index].bandejas < associadosSemBandeja[index].bandejaLimite) {
+                    associadosSemBandeja[index].bandejas += 1
+                    associadosSemBandeja[index].mudas += MUDAS_POR_BANDEJA
+                    loteDisponivel -= 1
+                    bandejaAdicionada = true
+                }
+                index++
+
+            }
+
+        }
+
+
+
+    }
+
+    const associadosDistribuidos: Distribuicao[] = distribuicaoAssociados.map(associado => {
+        const distribuido: Distribuicao = {
+            cnpj: associado.cnpj,
+            nome: associado.nome,
+            bandejas: associado.bandejas,
+            mudas: associado.mudas,
+            ...(associado.motivoExclusao && {
+                motivoExclusao: associado.motivoExclusao
+            })
+        }
+
+        return distribuido
+    })
+
+    const resultadoDistribuicao: ResultadoRateio = {
+        distribuicao: associadosDistribuidos,
+        totalDistribuido: totalMudas - (loteDisponivel * MUDAS_POR_BANDEJA) - sobraNaoDistribuida,
+        sobraNaoDistribuida: sobraNaoDistribuida + (loteDisponivel * MUDAS_POR_BANDEJA)
+    }
+    return resultadoDistribuicao
+}
---
+++ rateiro.test.ts
@@ -0,0 +1,167 @@
+import { describe, expect, it } from "vitest";
+import type { Associacao, Distribuicao, ResultadoRateio } from "./rateio";
+import { ratearMudas } from "./rateio";
+describe("Distribui corretamente os lotes entre as associações", () => {
+
+    it("Exemplo 1 - Saturação e redistribuição", () => {
+        const totalMudas = 18_000
+
+        const associacoes: Associacao[] = [
+            {
+                nome: "ASPRORIO",
+                municipio: "Espigao D Oeste",
+                familias: 120,
+                cotaMaxima: 4_000,
+                cnpj: "07.308.498/0001-01",
+                situacao: "regular"
+            },
+            {
+                nome: "APROPERO",
+                municipio: "Chupinguaia",
+                familias: 80,
+                cotaMaxima: 18_000,
+                cnpj: "15.893.829/0001-38",
+                situacao: "regular"
+            },
+            {
+                nome: "ARUVE",
+                municipio: "Cacoal",
+                familias: 80,
+                cotaMaxima: 18_000,
+                cnpj: "22.859.854/0001-60",
+                situacao: "regular"
+
+            },
+            {
+                nome: "Água Boa",
+                municipio: "Água Boa",
+                familias: 45,
+                cotaMaxima: 2_000,
+                cnpj: "34.537.183/0001-09",
+                situacao: "regular"
+            },
+            {
+                nome: "ACRUB",
+                municipio: "Cacoal",
+                familias: 35,
+                cotaMaxima: 18_000,
+                cnpj: "82.589.865/0001-08",
+                situacao: "suspensa"
+            },
+            {
+                nome: "Alto Alegre",
+                municipio: "Vale do Anari",
+                familias: 0,
+                cotaMaxima: 5_000,
+                cnpj: "02.785.883/0001-18",
+                situacao: "regular"
+            }
+        ]
+
+        const distribuicaoEsperada: Distribuicao[] = [
+            {
+                nome: associacoes[0].nome,
+                cnpj: associacoes[0].cnpj,
+                bandejas: 80,
+                mudas: 4_000
+            },
+            {
+                nome: associacoes[1].nome,
+                cnpj: associacoes[1].cnpj,
+                bandejas: 120,
+                mudas: 6_000
+            },
+            {
+                nome: associacoes[2].nome,
+                cnpj: associacoes[2].cnpj,
+                bandejas: 120,
+                mudas: 6_000
+            },
+            {
+                nome: associacoes[3].nome,
+                cnpj: associacoes[3].cnpj,
+                bandejas: 40,
+                mudas: 2_000
+            },
+            {
+                nome: associacoes[4].nome,
+                cnpj: associacoes[4].cnpj,
+                bandejas: 0,
+                mudas: 0,
+                motivoExclusao: associacoes[4].situacao
+            },
+            {
+                nome: associacoes[5].nome,
+                cnpj: associacoes[5].cnpj,
+                bandejas: 0,
+                mudas: 0
+            }
+        ]
+
+        const resultadoEsperado: ResultadoRateio = {
+            distribuicao: distribuicaoEsperada,
+            totalDistribuido: 18_000,
+            sobraNaoDistribuida: 0
+        }
+        expect(ratearMudas(totalMudas, associacoes)).toEqual(resultadoEsperado)
+
+    })
+
+    it("Maiores restos, sobra e ordenação", () => {
+        const totalMudas = 5_180
+        const associacoes: Associacao[] = [
+            {
+                nome: "Alto Alegre",
+                municipio: "Vale do Anari",
+                cnpj: "02.785.883/0001-18",
+                familias: 10,
+                cotaMaxima: 100_000,
+                situacao: "regular"
+            },
+            {
+                nome: "Água Boa",
+                municipio: "Água Boa",
+                familias: 10,
+                cotaMaxima: 100_000,
+                cnpj: "34.537.183/0001-09",
+                situacao: "regular"
+            },
+            {
+                nome: "Boa Esperança",
+                municipio: "Novo Mundo",
+                familias: 5,
+                cotaMaxima: 3_000,
+                cnpj: "25.027.055/0001-16",
+                situacao: "regular"
+            }
+        ]
+
+        const distribuicaoEsperada: Distribuicao[] = [
+            {
+                nome: associacoes[0].nome,
+                cnpj: associacoes[0].cnpj,
+                bandejas: 41,
+                mudas: 2_050
+            },
+            {
+                nome: associacoes[1].nome,
+                cnpj: associacoes[1].cnpj,
+                bandejas: 41,
+                mudas: 2_050
+            },
+            {
+                nome: associacoes[2].nome,
+                cnpj: associacoes[2].cnpj,
+                bandejas: 21,
+                mudas: 1_050
+            }
+        ]
+        const resultadoEsperado: ResultadoRateio = {
+            distribuicao:distribuicaoEsperada,
+            totalDistribuido: 5150,
+            sobraNaoDistribuida: 30
+        }
+
+        expect(ratearMudas(totalMudas, associacoes)).toEqual(resultadoEsperado)
+    })
+})
